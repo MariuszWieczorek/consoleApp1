a3c7d88d51d816658ca482947f0058ac80ba37ac
Mariusz Wieczorek <m.wieczorek1972@gmail.com>
2020-11-26 14:04:20 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// imie, urodzenie rok,mie,dsz,miejsce 
+// cześć ... urodziłeś się ... masz x lat
+
+namespace consoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int numberOfYears = 0;
+        
+            Console.WriteLine("Podaj swoje imię");
+            var name = Console.ReadLine();
+            
+            Console.WriteLine("Podaj rok urodzenia");
+            var yearOfBirth = getNumber();
+
+
+            Console.WriteLine("Podaj miesiąc urodzenia");
+            var monthOfBirth = getNumber();
+            
+            Console.WriteLine("Podaj dzień urodzenia");
+            int dayOfBirth = getNumber();
+
+            Console.WriteLine("Podaj miejsce urodzenia");
+            string placeOfBirth = Console.ReadLine();
+
+            numberOfYears = calculateNumberOfYears(yearOfBirth, monthOfBirth , dayOfBirth);
+            
+            if (numberOfYears > -1)
+            Console.WriteLine($"Cześć {name} masz {numberOfYears} lat \n urodziłeś się w {placeOfBirth}");
+            Console.ReadLine();
+        }
+
+        static int calculateNumberOfYears(int year, int month , int day)
+        {
+            DateTime currentDate = DateTime.Now;
+            int numberOfYears = 0;
+            try
+            {
+                DateTime date   = new DateTime(year, month, day);
+                TimeSpan interval = currentDate - date;
+                numberOfYears = interval.Days / 365;
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+
+                
+                string message = $"Z podanych parametów: rok {year}, \n miesiąc {month}, \n dzień  {day} \n nie można utworzyć prawidłowej daty";
+                //throw new Exception(message);
+                Console.WriteLine(message);
+                return -1;
+            }
+            return numberOfYears;
+        }
+
+        static int getNumber()
+        {
+            string input = Console.ReadLine();
+            if (!int.TryParse(input, out int number))
+            {
+                string message = $"wpisanych znaków {input} nie można przekonwertować na liczbę"; 
+                throw new Exception(message);
+            }
+            return number;
+        }
+        
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("consoleApp1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("consoleApp1")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dfb129b3-305c-44c8-bd3b-63e3e6153291")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
